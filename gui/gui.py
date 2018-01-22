dbbd7a8bd2f75cd6a8679cb8d87c40e346bf8caf
Floz <Floz_Pub@hotmail.com>
2018-01-22 20:58:54 +0800

    :construction: add gui

--- gui/gui.py
+++ gui/gui.py
@@ -2,30 +2,10 @@ import webbrowser
 from flask import Flask
 from flask import render_template
 import socket
+import threading
+import sys
 lock = threading.Lock()
-dialog = ''
-
-def listen():
-    global dialog
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('127.0.0.1', 9001))
-    s.listen(5)
-    while True:
-        sock, addr = s.accept()
-        lock.acquire()
-        dialog = ""
-        print('Accept new connection from %s:%s...' % addr)
-        while True:
-            data = sock.recv(1024)
-            if not len(buf)
-                break
-            print(buf)
-            dialog += buf
-        lock.release()
-
-        sock.close()
-        print('Connection from %s:%s closed.' % addr)
-
+dialog = 'test'
 
 app = Flask(__name__)
 
@@ -39,13 +19,33 @@ def query():
     global dialog
     lock.acquire()
     tmp = dialog
+    print(dialog, file=sys.stdout)
     lock.release()
     return tmp
 
+def flaskThread():
+    app.run()
+
 if __name__ == '__main__':
-    t = threading.Thread(target=listen)
+    t = threading.Thread(target=flaskThread, args=())
     t.start()
+
     chrome = webbrowser.get('Chrome')
     chrome.open('http://127.0.0.1:5000/')
-    app.run(debug = True)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('127.0.0.1', 9002))
+    s.listen(5)
+    while True:
+        sock, addr = s.accept()
+        lock.acquire()
+        dialog = ""
+        print('Accept new connection from %s:%s...' % addr)
+        while True:
+            buf = sock.recv(1024)
+            if not len(buf):
+                break
+            dialog += buf.decode("utf-8")
+        lock.release()
 
+        sock.close()
+        print('Connection from %s:%s closed.' % addr)
