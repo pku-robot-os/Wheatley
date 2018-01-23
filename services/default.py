b269a40594672a3686b813c4da33ecc0f37bbb88
Floz <Floz_Pub@hotmail.com>
2018-01-23 20:49:13 +0800

    :bug: change api

--- services/default.py
+++ services/default.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__=="__main__":
 	s = input()
-	p = {'key':'486dba631b5c438ca1e8f419e61eab3e', 'info':s, 'userid':'204842'}
+	p = {'key':'43f0f552df74488580a4a7b06af4a134', 'info':s, 'userid':'204842'}
 	r = requests.post('http://www.tuling123.com/openapi/api', params=p)
 	j = json.loads(r.text)
 	print(j['text'])
